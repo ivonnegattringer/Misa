69e33ecc2ae5c3d59868465ab5138bcd3f4892a4
AlexanderKapsammer <kapsammeralex@gmail.com>
2020-12-14 09:56:56 +0100

    added hosting

--- MisaKitchen/src/environments/environment.prod.ts
+++ MisaKitchen/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCr-WzotWlzZfOamV_PkWURkDlxUtnIH6A",
+    authDomain: "misa-2021.firebaseapp.com",
+    databaseURL: "https://misa-2021.firebaseio.com",
+    projectId: "misa-2021",
+    storageBucket: "misa-2021.appspot.com",
+    messagingSenderId: "629756915148",
+    appId: "1:629756915148:web:a47bc8ddbcb82a76e18169"
+  }
 };
